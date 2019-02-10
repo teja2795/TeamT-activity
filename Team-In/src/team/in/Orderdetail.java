5ddde51733f3c7a1d97cb8949402ababa8ba1267
santhosh <chakravarthy.janyavula@gmail.com>
2019-02-10 09:20:03 -0500

    Orderdetail  methods commit

--- Team-In/src/team/in/Orderdetail.java
+++ Team-In/src/team/in/Orderdetail.java
@@ -12,6 +12,19 @@ package team.in;
 public class Orderdetail {
     int quantity;
     String taxstatus;
-    
+    public void calcSubTotal ()
+    {
+        System.out.println("this is calcSubTotal in Orderdetail class");
+    }
+    public void calcWeight ()
+    {
+        System.out.println("this is calcWeight in Orderdetail class");
+    }
+    public void calcTax ()
+    {
+        System.out.println("this is calTax in Orderdetail class");
+    }
+    Order oo = new Order();
+    Item i= new Item();
     
 }
