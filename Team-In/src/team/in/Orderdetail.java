effba1f4bb9ecf0286a8ac7e347d1edb66067235
santhosh <chakravarthy.janyavula@gmail.com>
2019-02-10 08:56:08 -0500

    Order detail initial commit

---
+++ Team-In/src/team/in/Orderdetail.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.in;
+
+/**
+ *
+ * @author temp
+ */
+public class Orderdetail {
+    int quantity;
+    String taxstatus;
+    
+    
+}
