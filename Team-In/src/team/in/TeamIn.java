9c200c8cde4a9b95d15f0d9e2891d3ffd7e466f1
vikram.sumanth999@gmail.com <swaranbachu@gmail.com>
2019-02-10 09:44:51 -0500

    TeamIn class commit

--- Team-In/src/team/in/TeamIn.java
+++ Team-In/src/team/in/TeamIn.java
@@ -16,6 +16,23 @@ public class TeamIn {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Order o=new Order();
+        o.calcSubTotal();
+        o.calcTax();
+        o.calcTotal();
+        o.calcTotalWeight();
+        Orderdetail oo=new Orderdetail();
+        oo.calcSubTotal();
+        oo.calcTax();
+        oo.calcWeight();
+        Item i=new Item();
+        i.Instock();
+        i.getPriceForQuantity();
+        i.getTax();
+        Check c=new Check();
+        c.authorized();
+        Credit cc=new Credit();
+        cc.authorized();
     }
     
 }
