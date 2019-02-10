14a27cdbac8aa9b7f274fd4d798c975406ceec48
1894438 <1894438@E21042.CEGEPGIM.LCL>
2019-02-10 09:10:37 -0500

    credit Class Commit

--- Team-In/src/team/in/Credit.java
+++ Team-In/src/team/in/Credit.java
@@ -18,7 +18,7 @@ public class Credit {
     
     public void authorized(){
        
-        system.out.println("this is autherization in Credit subclass")
+        system.out.println("this is autherization in Credit subclass");
     }
             
 }
